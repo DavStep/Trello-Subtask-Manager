cada4934db498d3a2dd7eaaed28b3d6a5c882b83
stepdav <davitstepanyan99@gmail.com>
2025-04-15 21:30:43 +0400

    test

---
+++ client.js
@@ -0,0 +1,27 @@
+window.TrelloPowerUp.initialize({
+  'card-buttons': function (t) {
+    return [{
+     icon: 'https://trello-subtask-manager.netlify.app/logo.png',
+      text: 'Add Subtask',
+      callback: function (t) {
+        return t.popup({
+          title: 'Add Subtask',
+          url: 'subtask-picker.html',
+          height: 100
+        });
+      }
+    }];
+  },
+
+  'card-detail-sections': function (t) {
+    return [{
+      title: 'Subtasks',
+      icon: 'https://trello-subtask-manager.netlify.app/logo.png',
+      content: {
+        type: 'iframe',
+        url: t.signUrl('subtask-section.html'),
+        height: 150
+      }
+    }];
+  }
+});
